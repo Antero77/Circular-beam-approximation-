ae00f3f0f71dc4c6a7739d53724173029f6056dc
Antero77 <122158447+Antero77@users.noreply.github.com>
2025-07-26 16:19:33 +0300

    Update and rename calc2_PDT_with_determin.py to Figure_6a.py

--- Figure_6a.py
+++ Figure_6a.py
@@ -1,10 +1,14 @@
 import numpy as np
 import scipy as sp
 from pyatmosphere import gpu
+import seaborn as sns
+import matplotlib.pyplot as plt
+import circular_beam
+from pyatmosphere import QuickChannel, measures
+
 
 gpu.config['use_gpu'] = True
 
-import matplotlib.pyplot as plt
 
 plt.rcParams['axes.axisbelow'] = True
 plt.rcParams["font.family"] = "DejaVu Serif"
@@ -21,21 +25,10 @@ save_kwargs = {
 
 
 
-import seaborn as sns
-import datetime
-
-import circular_beam
-
-from pyatmosphere.theory.pdt import beam_wandering_pdt
 
 
-### QuickChannel example
 
-then = datetime.datetime.now()
-from pyatmosphere import QuickChannel, measures
-
-
-l = 2000
+l = 1000
 g=-15
 Cn2 =  10**g
 beam_w0 = (l * 8.08 * 10 ** (-7) / np.pi) ** 0.5
@@ -80,7 +73,6 @@ for i in range(n):
 
     sumx2_0 = sumx2_0 + (measures.mean_x(quick_channel, output=output)) ** 2
 
-    # temp = temp + (measures.mean_x2(quick_channel, output=output)) ** 2
     temp_etha=measures.eta(quick_channel, output=quick_channel.run())
     etha.append(temp_etha)
     sum_etha=sum_etha+temp_etha
@@ -112,17 +104,11 @@ analy_W4 = 16* (quick_channel.source.w0 ** 4 * omega ** (-4) /16 +
                0.5 * analy_W2 * analy_x2_0 - 3 * analy_x2_0 ** 2)
 
 
-print("x2_0 analy=",analy_x2_0, "x2_0 sim=", sim_x2_0)
-print("W2 analy=",analy_W2, "W2 sim=", sim_W2)
-print("W4 analy=",analy_W4, "W4 sim=", sim_W4)
 # --------------------------------------
 
-"""
-initial analytical
-analy_etha=1-np.exp(-2*quick_channel.pupil.radius**2/(analy_W2+4*analy_x2_0))
-analytical with local approx
-"""
-th=0.136*popravka_ro*quick_channel.get_rythov2()*omega**(-5/6)
+#local approximation can be used
+# th = 0.136 * popravka_ro * quick_channel.get_rythov2() * omega ** (-5 / 6)
+th = 0 
 analy_etha=np.exp(-th)*(1-np.exp(-quick_channel.pupil.radius**2*omega**2/quick_channel.source.w0**2/(0.5+5*th)))
 
 
@@ -135,20 +121,18 @@ second_mn=1-np.exp(-4*omega**2*quick_channel.pupil.radius**2/quick_channel.sourc
 
 analy_etha2=norm*first_mn*second_mn
 
-print("etha analy=",analy_etha, "etha sim=", sim_etha)
-print("etha2 analy=",analy_etha2, "etha2 sim=", sim_etha2)
 #-----------------------------------------------------------
 determ_losses=10**(-(3+0.1*(quick_channel.path.length/1000))/10)
 #--------------------------------------------------------------
 etha=[x * determ_losses for x in etha]
 ax = sns.histplot(etha, bins=200, kde=False, element="step", stat='density', color='C0')
 
-number_for_dots_pdt=600
+number_for_dots_pdt=200
 
 #--------------------------------------------------------------
 
 t = np.linspace(0.00001, 1, num=number_for_dots_pdt)
-#t = np.linspace(0.00001, 0,8, num=number_for_dots_pdt)
+
 t_another=[x * determ_losses for x in t]
 
 
@@ -219,82 +203,8 @@ plt.plot(t, pdt_anc_analy_hiddenloss, color='#69247C',linewidth='2', linestyle='
 
 ax.grid()
 ax.set(xlabel=r'Transmittance $\eta$', ylabel='Probability density')
-ax.set_xlim(left=0, right=0.35)
-#ax.set_xlim(left=0, right=1)
-plt.savefig("etha_distrib.pdf", **save_kwargs)
-
-#plt.show()
-
-
-"""
-fig, ax = plt.subplots(1, 1)
-
-
-res = sp.stats.ecdf(etha)
-res.cdf.plot(ax, color='C0',linewidth='2')
-
-
-cumul_sim_anc_direct=[]
-cumul_analy_anc_direct=[]
-
-cumul_sim_anc_hidden=[]
-cumul_analy_anc_hidden=[]
-
-spec_t=t[1::2]
-spec_pdt_circ_sim=pdt_circ_sim[1::2]
-spec_pdt_circ_analy=pdt_circ_analy[1::2]
-spec_pdt_anc_sim=pdt_anc_sim[1::2]
-spec_pdt_anc_analy=pdt_anc_analy[1::2]
-
-
-sum_sim_anc_direct=0
-sum_analy_anc_direct=0
-sum_sim_anc_hidden=0
-sum_analy_anc_hidden=0
-count=0
-for i in spec_t:
-    count=count+1
-    sum_sim_anc_direct= sum_sim_anc_direct+spec_pdt_circ_sim[count-1]
-    sum_analy_anc_direct=sum_analy_anc_direct+spec_pdt_circ_analy[count-1]
-    sum_sim_anc_hidden=sum_sim_anc_hidden+spec_pdt_anc_sim[count-1]
-    sum_analy_anc_hidden=sum_analy_anc_hidden+spec_pdt_anc_analy[count-1]
-
-    cumul_sim_anc_direct.append(sum_sim_anc_direct * (i  - 0.00001) / count)
-    cumul_analy_anc_direct.append(sum_analy_anc_direct * (i  - 0.00001) / count)
-    cumul_sim_anc_hidden.append(sum_sim_anc_hidden * (i  - 0.00001) / count)
-    cumul_analy_anc_hidden.append(sum_analy_anc_hidden * (i  - 0.00001) / count)
-
-
-
-
-
-
-ax.step(spec_t, cumul_sim_anc_direct, 'r', linewidth='2')
-ax.step(spec_t, cumul_analy_anc_direct, 'green', linewidth='2')
-ax.step(spec_t, cumul_sim_anc_hidden, 'orange', linestyle='dashed', linewidth='2')
-ax.step(spec_t, cumul_analy_anc_hidden, 'violet', linestyle='dashed', linewidth='2')
-
-
-
-
-ax.grid()
-ax.set(xlabel=r'Transmittance $\eta$', ylabel='Cumulative distribution')
-#ax.set_xlim(left=0.8, right=1)
+#ax.set_xlim(left=0, right=0.35)
 ax.set_xlim(left=0, right=1)
-plt.savefig("etha_cumul_distrib.pdf", **save_kwargs)
-
-"""
-
-"""
-beam_result = simulations.BeamResult(quick_channel)
-pdt_result = simulations.PDTResult(quick_channel)
-sim = simulations.Simulation([beam_result, pdt_result])
-sim.run(plot_step=100)
-"""
-
-# ------------------
-now = datetime.datetime.now()
-delta = now - then
-print(delta.seconds / 60, "min")
+plt.savefig("etha_distrib_determ.pdf", **save_kwargs)
 
 
